291f62d9c7260c1d52dda5c548f3f16294d95aad
Chris Wald <wald.j.chris@gmail.com>
2020-01-15 20:02:50 -0600

    Add test rig to make running from source easier

---
+++ Test Harness/Program.cs
@@ -0,0 +1,21 @@
+﻿using IIS_Active_Passive_Failover;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Harness
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+			configMap.ExeConfigFilename = @"IIS Active-Passive Failover.exe.config";
+			Service1 service = new Service1();
+			service.ExternalRun(configMap);
+		}
+	}
+}
