210260fa35cfbe616e52e4339079178f48bb477c
Chris Wald <wald.j.chris@gmail.com>
2020-01-21 19:45:37 -0600

    Add installer and uninstaller

--- IIS Active-Passive Failover/HealthCheckConfig.cs
+++ IIS Active-Passive Failover/HealthCheckConfig.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace IIS_Active_Passive_Failover
 {
